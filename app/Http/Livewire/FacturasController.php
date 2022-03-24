6ff09c13db2d1feeb634920577fc6e96d08a262c
luismariovaldiviesos <luismario.valdiviesos@gmail.com>
2022-03-24 14:53:22 -0500

    empezamos desde aqui  el buscador de clientes

--- app/Http/Livewire/FacturasController.php
+++ app/Http/Livewire/FacturasController.php
@@ -10,9 +10,8 @@ class FacturasController extends Component
 
     public function render()
     {
-
-        return view('livewire.facturas.component'
-        )->extends('layouts.theme.app')
+        $empresa =  Empresa::all();
+        return view('livewire.facturas.component', compact('empresa'))->extends('layouts.theme.app')
 		->section('content');
 
     }
--- resources/views/livewire/facturas/component.blade.php
+++ resources/views/livewire/facturas/component.blade.php
@@ -1,125 +1,45 @@
 <div class="row sales layout-top-spacing">
 
     <div class="col-sm-12">
-
         <div class="widget widget-chart-one">
             <div class="widget-heading">
                 <h4 class="card-title">
-
+                    {{ $empresa[0]->razonSocial }}
                 </h4>
-
             </div>
-
-
             <div class="widget-content">
                 @can('ver_empresa')
-
-
                 <div class="row">
-                    <div class="col-sm-12 col-md-3">
-                        <div class="form-group">
-                            <label >Razón Social</label>
-                            <input type="text" wire:model.lazy="razonSocial" class="form-control">
-                            @error('razonSocial') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 col-md-3">
-                        <div class="form-group">
-                            <label >Nombre Comercial</label>
-                            <input type="text" wire:model.lazy="nombreComercial" class="form-control">
-                            @error('nombreComercial') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
                     <div class="col-sm-12 col-md-2">
                         <div class="form-group">
                             <label >RUC</label>
-                            <input type="text" wire:model.lazy="ruc" class="form-control" placeholder="ej:0101049643001" maxlength="13">
-                            @error('ruc') <span class="text-danger er">{{ $message }}</span> @enderror
+                            <h5>{{ $empresa[0]->ruc }}</h5>
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-2">
                         <div class="form-group">
-                            <label >Establecimiento</label>
-                            <input type="text" wire:model.lazy="estab" class="form-control" placeholder="ej:001" maxlength="3">
-                            @error('estab') <span class="text-danger er">{{ $message }}</span> @enderror
+                            <label >Direccion Matriz</label>
+                            <h5>{{ $empresa[0]->dirMatriz }}</h5>
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-2">
                         <div class="form-group">
-                            <label >Pto Emision</label>
-                            <input type="text" wire:model.lazy="ptoEmi" class="form-control" placeholder="ej:001" maxlength="3">
-                            @error('ptoEmi') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 col-md-3">
-                        <div class="form-group">
-                            <label >Matriz</label>
-                            <input type="text" wire:model.lazy="dirMatriz" class="form-control">
-                            @error('dirMatriz') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-3">
-                        <div class="form-group">
-                            <label >Sucursal</label>
-                            <input type="text" wire:model.lazy="dirEstablecimiento" class="form-control">
-                            @error('dirEstablecimiento') <span class="text-danger er">{{ $message }}</span> @enderror
+                            <label >Direccion Sucursal</label>
+                            <h5>{{ $empresa[0]->dirEstablecimiento }}</h5>
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-2">
                         <div class="form-group">
                             <label >Teléfono</label>
-                            <input type="text" wire:model.lazy="telefono" class="form-control" maxlength="10">
-                            @error('telefono') <span class="text-danger er">{{ $message }}</span> @enderror
+                            <h5>{{ $empresa[0]->telefono }}</h5>
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-2">
                         <div class="form-group">
-                            <label >Email</label>
-                            <input type="text" wire:model.lazy="email" class="form-control">
-                            @error('email') <span class="text-danger er">{{ $message }}</span> @enderror
+                            <label >Correo</label>
+                            <h5>{{ $empresa[0]->email }}</h5>
                         </div>
                     </div>
-                    <div class="col-sm-12 col-md-2">
-                        <div class="form-group">
-                            <label >Ambiente</label>
-                            <input type="text" wire:model.lazy="ambiente" class="form-control" maxlength="1">
-                            @error('ambiente') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 col-md-2">
-                        <div class="form-group">
-                            <label >Emisión</label>
-                            <input type="text" wire:model.lazy="tipoEmision" class="form-control" maxlength="1">
-                            @error('tipoEmision') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-2">
-                        <div class="form-group">
-                            <label >Cont Espcial</label>
-                            <input type="text" wire:model.lazy="contribuyenteEspecial" class="form-control">
-                            @error('contribuyenteEspecial') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-2">
-                        <div class="form-group">
-                            <label >Contabilidad</label>
-                            <input type="text" wire:model.lazy="obligadoContabilidad" class="form-control" maxlength="2">
-                            @error('obligadoContabilidad') <span class="text-danger er">{{ $message }}</span> @enderror
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 col-md-6">
-                        <div class="form-group">
-                            <label >Logo</label>
-                            <input type="file" wire:model.lazy="logo"  accept="image/x-png, image/jpeg, image/gif" class="form-control">
-                            @error('logo') <span class="text-danger ">{{ $message }}</span> @enderror
-
-                        </div>
-                    </div>
-
                 </div>
 
             </div>
@@ -129,19 +49,34 @@
         <br>
 
         @endcan
-        <div>
 
-            @can('editar_empresa')
-            <button type="button" wire:click.prevent="Guardar()" class="btn btn-dark close-modal">
-                Guardar
-                </button>
-            @endcan
 
+
+
+
+
+    </div>
+
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+
+            <div class="widget-heading">
+                <h4 class="card-title">
+                   Datos Cliente
+                </h4>
+            </div>
 
         </div>
+    </div>
 
 
+    <div>
 
+        @can('editar_empresa')
+        <button type="button" wire:click.prevent="Guardar()" class="btn btn-dark close-modal">
+            Guardar
+            </button>
+        @endcan
 
 
     </div>
